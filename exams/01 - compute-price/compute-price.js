fd4429fe8dfa6f43f5bdfcfec1460432fd74430c
ibrahimdans <hibrahim69@live.fr>
2018-12-18 15:22:03 +0100

    refactor

--- exams/01 - compute-price/compute-price.js
+++ exams/01 - compute-price/compute-price.js
@@ -1,32 +1,29 @@
 export default function computePrice(orders) {
-
-    
   if (orders.length > 6) {
-      throw new Error("Too many orders!!!@@");
+    throw new Error("Too many orders!!!@@");
   }
-  if(orders.length ===0){
-      throw new Error('Please give a array not empty')
+  if (orders.length === 0) {
+    throw new Error("Please give a array not empty");
   }
   if (Array.isArray(orders)) {
     let total = 0;
     for (const item in orders) {
-
       if (orders[item].price < 0 || orders[item].quantity < 0) {
-          throw new Error("Negative values are forbidden");
+        throw new Error("Negative values are forbidden");
       }
       if (orders[item].price === 0 || orders[item].quantity === 0) {
-          throw new Error("Zero values are forbidden.");
+        throw new Error("Zero values are forbidden.");
       }
       if (
         orders[item].price === undefined ||
         orders[item].quantity === undefined
       ) {
-          throw new Error("Invalid order");
+        throw new Error("Invalid order");
       }
       total += orders[item].price * orders[item].quantity;
     }
     return total;
   }
 
-    throw new Error("Incorrect type");
+  throw new Error("Incorrect type");
 }
