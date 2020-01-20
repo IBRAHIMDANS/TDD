44c9ef1769fc4c96daa20f1b9b375d05331e6c35
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-01-20 14:42:43 +0100

    tennis V2

--- src/tennis.js
+++ src/tennis.js
@@ -1,28 +1,35 @@
 export class TennisGame {
 
-    constructor(playerOne, playerTwo) {
-        this.playerOne = {name: playerOne, score: 0};
-        this.playerTwo = {name: playerTwo, score: 0};
+    constructor(playerOneName, playerTwoName) {
+        this.playerOne = {name: playerOneName, score: 0};
+        this.playerTwo = {name: playerTwoName, score: 0};
     }
 
     displayScore() {
-        if (this.playerTwo.score === 0 && this.playerOne.score === 0) {
-            return `${this.selectScore(0)}-${this.selectScore(0)}`
-        } else if (this.playerOne.score === 4 && this.playerTwo.score === 3) {
-            return `Avantage: ${this.playerOne.name}`
-        }else if (this.playerOne.score === 3 && this.playerTwo.score === 4) {
-            return `Avantage: ${this.playerTwo.name}`
-        } else if (this.playerTwo.score === this.playerTwo.score) {
+        if (this.playerOne.score === 4 && this.playerTwo.score === 3) {
+            return `Avantage=> ${this.playerOne.name}.`
+        } else if (this.playerOne.score === 3 && this.playerTwo.score === 4) {
+            return `Avantage=> ${this.playerTwo.name}.`
+        } else if (this.playerOne.score >= 5) {
+            return `Nous avons un gagnant: ${this.playerOne.name}.`
+        }else if (this.playerTwo.score >= 5) {
+            return `Nous avons un gagnant: ${this.playerTwo.name}.`
+        }else if(this.playerOne.score === 4 && this.playerTwo.score === 4) {
+            this.playerOne.score--;
+            this.playerTwo.score--;
+            return 'deuce'
+        } else if (this.playerTwo.score === 3 && this.playerOne.score === 3) {
             return 'deuce'
         }
+         return `${this.selectScore(this.playerOne.score)}-${this.selectScore(this.playerTwo.score)}`
     }
 
     scorePlayerOne() {
-       this.playerTwo.score++
+       this.playerOne.score = this.playerOne.score+1
     }
 
     scorePlayerTwo() {
-        this.playerTwo.score++
+        this.playerTwo.score = this.playerTwo.score+1
     }
 
     selectScore(score) {
--- tests/tennis.spec.js
+++ tests/tennis.spec.js
@@ -1,8 +1,10 @@
-import { TennisGame} from "../src/tennis";
+import {TennisGame} from "../src/tennis";
 
 
-describe('Tenis Game', () => {
-    const game = new TennisGame('ibra','hima');
+describe('Tennis Game', () => {
+    const playerOne = 'Ibra';
+    const playerTwo = 'Hima';
+    const game = new TennisGame(playerOne, playerTwo);
 
     test('it returns 0-0 because is start game ', () => {
         expect(game.displayScore()).toBe('0-0')
@@ -34,8 +36,25 @@ describe('Tenis Game', () => {
     test('display score for deuce', () => {
         expect(game.displayScore()).toBe('deuce')
     });
+    test('add  to score playerTwo', () => {
+        expect(game.scorePlayerTwo()).toBe()
+    });
+    test('display score return avantage', () => {
+        expect(game.displayScore()).toBe(`Avantage=> ${playerTwo}.`)
+    });
+    test('add  to score playerTwo', () => {
+        expect(game.scorePlayerOne()).toBe()
+    });
     test('display score for deuce', () => {
         expect(game.displayScore()).toBe('deuce')
     });
-
+    test('add  to score playerTwo', () => {
+        expect(game.scorePlayerTwo()).toBe()
+    });
+    test('add  to score playerTwo', () => {
+        expect(game.scorePlayerTwo()).toBe()
+    });
+    test('display score for deuce', () => {
+        expect(game.displayScore()).toBe(`Nous avons un gagnant: ${playerTwo}.`)
+    });
 });
